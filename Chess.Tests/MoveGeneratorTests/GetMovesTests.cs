c3adce47a6a53e462cd3005c7b575c9d9ede66b5
Lowell Smith <lowell@farmworks.com>
2015-12-30 15:27:22 -0500

    Improved legal moves of king, preventing it from moving into an attacked square, and only castling while in, through, or into check.

--- Chess.Tests/MoveGeneratorTests/GetMovesTests.cs
+++ Chess.Tests/MoveGeneratorTests/GetMovesTests.cs
@@ -8,18 +8,18 @@ namespace Chess.Tests.MoveGeneratorTests
 	[TestClass]
 	public class GetMovesTests
 	{
+		//From http://chessprogramming.wikispaces.com/Perft+Results
+		//Depth  Nodes      Captures  E.p.   Castles  Promotions  Checks   Checkmates
+		//1      48         8         0      2        0           0        0
+		//2      2039       351       1      91       0           3        0
+		//3      97862      17102     45     3162     0           993      1
+		//4      4085603    757163    1929   128013   15172       25523    43
+		//5      193690690  35043416  73365  4993637  8392        3309887  30171
+		private readonly MoveGenerator target = new MoveGenerator(GameState.FromForsythEdwardsNotation("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -"));
+
 		[TestMethod]
 		public void Test_MoveGenerator_GetMoves()
 		{
-			//From http://chessprogramming.wikispaces.com/Perft+Results
-			//Depth  Nodes      Captures  E.p.   Castles  Promotions  Checks   Checkmates
-			//1      48         8         0      2        0           0        0
-			//2      2039       351       1      91       0           3        0
-			//3      97862      17102     45     3162     0           993      1
-			//4      4085603    757163    1929   128013   15172       25523    43
-			//5      193690690  35043416  73365  4993637  8392        3309887  30171
-			var state = GameState.FromForsythEdwardsNotation("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
-			var target = new MoveGenerator(state);
 			var actual = target.GetMoves()
 				.OrderBy(o => o.From)
 				.ThenBy(o => o.To)
@@ -79,11 +79,78 @@ namespace Chess.Tests.MoveGeneratorTests
 		}
 
 		[TestMethod]
+		public void Expect_king_doesnt_move_to_attacked_cell()
+		{
+			target.MakeMove(new Move(Cell.e5, Cell.g6));
+			var moves = target.GetMoves();
+
+			Assert.IsFalse(moves.Contains(new Move(Cell.e8, Cell.f8)));
+		}
+
+		[TestMethod]
+		public void Expect_king_cant_castle_through_check_kingside()
+		{
+			target.MakeMove(new Move(Cell.e5, Cell.g6));
+			var moves = target.GetMoves();
+
+			Assert.IsFalse(moves.Contains(new Move(Cell.e8, Cell.g8)));
+		}
+
+		[TestMethod]
+		public void Expect_king_cant_castle_into_check_kingside()
+		{
+			target.MakeMove(new Move(Cell.e5, Cell.g4));
+			target.MakeMove(new Move(Cell.a8, Cell.b8));
+			target.MakeMove(new Move(Cell.g4, Cell.h6));
+			var moves = target.GetMoves();
+
+			Assert.IsFalse(moves.Contains(new Move(Cell.e8, Cell.g8)));
+		}
+
+		[TestMethod]
+		public void Expect_king_cant_castle_through_check_queenside()
+		{
+			target.MakeMove(new Move(Cell.e5, Cell.c6));
+			var moves = target.GetMoves();
+
+			Assert.IsFalse(moves.Contains(new Move(Cell.e8, Cell.c8)));
+		}
+
+		[TestMethod]
+		public void Expect_king_cant_castle_into_check_queenside()
+		{
+			target.MakeMove(new Move(Cell.e2, Cell.a6));
+			var moves = target.GetMoves();
+
+			Assert.IsFalse(moves.Contains(new Move(Cell.e8, Cell.c8)));
+		}
+
+		[TestMethod]
+		public void Expect_king_cant_castle_while_in_check()
+		{
+			target.MakeMove(new Move(Cell.d5, Cell.e6));
+			target.MakeMove(new Move(Cell.a6, Cell.b7));
+			target.MakeMove(new Move(Cell.e6, Cell.f7));
+			var moves = target.GetMoves();
+
+			Assert.IsFalse(moves.Contains(new Move(Cell.e8, Cell.c8)));
+			Assert.IsFalse(moves.Contains(new Move(Cell.e8, Cell.g8)));
+		}
+
+		[TestMethod]
+		public void Expect_king_cant_capture_protected_piece()
+		{
+			target.MakeMove(new Move(Cell.d5, Cell.e6));
+			target.MakeMove(new Move(Cell.a6, Cell.b7));
+			target.MakeMove(new Move(Cell.e6, Cell.f7));
+			var moves = target.GetMoves();
+
+			Assert.IsFalse(moves.Contains(new Move(Cell.e8, Cell.f7)));
+		}
+
+		[TestMethod]
 		public void GetMoves_Depth_1_Tests()
 		{
-			var state = GameState.FromForsythEdwardsNotation("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
-			var target = new MoveGenerator(state);
-
 			var moves = target.GetMoves();
 
 			var totalMoves = 0;
